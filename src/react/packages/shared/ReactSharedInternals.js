a0f8f8c90fc35bbaa31e659203b2b961d1835a61
qiuxiaofeng <qiuxiaofeng@tapai.tv>
2020-12-23 10:43:28 +0800

    feat: 搭建本地react调试环境

---
+++ src/react/packages/shared/ReactSharedInternals.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+//  注释掉
+// import * as React from 'react';
+//  注释掉
+// const ReactSharedInternals =
+//   React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+
+// 修改
+import ReactSharedInternals from "../react/src/ReactSharedInternals";
+
+export default ReactSharedInternals;
